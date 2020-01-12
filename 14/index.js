e63e9ca79709b893d6c4412ff648998309c19aaa
Tatjana Zinko <ztvproweb@gmail.com>
2020-01-12 14:44:11 +0200

    Create first server

--- 14/index.js
+++ 14/index.js
@@ -1,2 +1,23 @@
-//Урок 14
+//Урок 14 Создание сервера
+
 const http = require('http');
+const fs = require('fs');
+
+http.createServer( function( req, res ){
+    if( req.url === '/' ) {
+        res.write('Hello World');
+        res.end();
+    } else if( req.url === '/about' ) {
+        res.write('Hello World!');
+        res.end();
+        console.log(req);
+    } else if( req.url === '/contact' ) {
+        fs.readFile('index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+          });
+    } 
+}).listen(3000, function(){
+    console.log( 'Server http://localhost:3000' );
+})
