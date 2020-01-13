beeb32f0e413d28628e2e941a40eb3abf49ba574
Tatjana Zinko <ztvproweb@gmail.com>
2020-01-13 20:15:01 +0200

    correction of displaying

--- 14/parser.js
+++ 14/parser.js
@@ -4,13 +4,18 @@ const fs = require('fs');
 
 let url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3';
 let body = '';
+let data = '';
 
 https.get(url,(res) => {
 
     res.on("data", function(chunk) {
         body += chunk;
         json = JSON.parse(body);
-        console.log(json);
+        for(let i = 0; i<json.length; i++) {
+            //console.log('Курс ' + json[i].ccy + ' равен: купля - ' + json[i].buy + ' продажа - ' + json[i].sale);
+            data += 'Курс ' + json[i].ccy + ' равен: купля - ' + json[i].buy + ' продажа - ' + json[i].sale + '\n';
+        }        
+        console.log(data);
     });    
 
 }).on("error", (error) => {
@@ -18,8 +23,9 @@ https.get(url,(res) => {
 });
 
 
- http.createServer( function( req, res ){  
-    res.write(body);
+ http.createServer( function( req, res ){ 
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'}); 
+    res.write(data);
     res.end();
  }).listen(3000, function(){
      console.log( 'Server http://localhost:3000' );
